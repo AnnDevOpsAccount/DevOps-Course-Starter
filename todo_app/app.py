0d417830787fd9b608fe4b0cc049b71447f2e0a4
AnnDevOpsAccount <anndavey.uk@gmail.com>
2022-02-21 16:08:38 +0000

    get items from Trello

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,13 +1,27 @@
 from flask import Flask, render_template, request
 from todo_app.data import session_items
 from todo_app.flask_config import Config
+import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config())
+url = "https://api.trello.com/1/boards/6212689e46f57218f07af552/lists/open"
 
 @app.route('/')
 def index():
-    items = session_items.get_items()
+    querystring = { "key": os.getenv("TRELLO_BOARD_KEY"),
+                    "token":os.getenv("TRELLO_BOARD_TOKEN"),
+                    "cards":"open"}
+    response = requests.request("GET", url, params=querystring)
+    response_json = response.json() 
+    items = []
+    for trello_list in response_json:
+        for card in trello_list['cards']:
+            print (card)
+            card['status'] = trello_list['name']
+            card['title'] = card ['name']
+            items.append(card)
     return render_template('index.html', items=items)
 
 @app.route('/addTask', methods=['POST'])
@@ -22,3 +36,11 @@ def end_task():
     item_to_Update.update({"status":"complete"})
     session_items.save_item(item_to_Update)
     return index()
+
+
+
+
+
+
+
+   
